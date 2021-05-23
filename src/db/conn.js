a30050bfc45a182ea2a2c7bf4fd1b4b5bbd79e10
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-05-23 21:59:18 +0530

    initial commite

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const express = require("express");
 const uri = process.env.MONGOOSE_URI || "mongodb://localhost:27017/jwt";
-const app = express();
 mongoose.connect(uri,
     {
         useCreateIndex: true,
